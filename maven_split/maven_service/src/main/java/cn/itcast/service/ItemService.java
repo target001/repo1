c3d5bb282e0d84146aacedde24345f8275cec05e
admin <admin@qq.com>
2019-06-11 11:53:25 +0800

    删除

--- maven_split/maven_service/src/main/java/cn/itcast/service/ItemService.java
+++
@@ -1,9 +0,0 @@
-package cn.itcast.service;
-
-import cn.itcast.domain.Items;
-
-
-public interface ItemService {
-
-    Items findOne(Integer id);
-}
--- maven_split/maven_web/src/main/webapp/index.jsp
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-<h2>Hello World!</h2>
-<a href="item/findOne?id=1">aaaaaaaaaa</a>
-</body>
-</html>
